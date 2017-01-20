63467a4c0db0a100d56f7bbbed0b9381c7cb9791
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-01-20 13:52:31 -0800

    numbers divisible by 15 will display as Ping-Pong

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,18 @@ $(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var num = $("input").val();
-    var countList = [1];
+    var countList = ["1, "];
     for(var i = 2; i <= num; i++){
       var output = i;
 
-      if(output%3 ===0) {
-        output = "Ping";
+      if(output%3 ===0 && output%5 ===0) {
+        output = "Ping-Pong";
       } else if (output%5 === 0){
         output = "Pong";
+      } else if (output%3 === 0){
+        output = "Ping";
       }
-      countList += output;
+      countList += output + ", ";
     }
 
     alert(countList.split("").join(", "));
